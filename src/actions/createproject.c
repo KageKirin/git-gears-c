96ffcb2d1e5b46cd9745b935f9428f9633399292
KageKirin <kagekirin@gmail.com>
2020-01-23 15:28:54 +0900

    add source files for initial set of actions

---
+++ src/actions/createproject.c
@@ -0,0 +1,10 @@
+#include "gears_util.h"
+
+int CreateProject(int argc, char** argv)
+{
+	gears_println("foobar doing stuff", NULL);
+	//getopt or my own implementation to parse arguments
+	//implementation of action
+	//result display
+	return 0;
+}
