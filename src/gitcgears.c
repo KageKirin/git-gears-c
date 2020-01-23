18b4dbf825b7949588a4d6e84cbcaa8ca9ed15b1
KageKirin <kagekirin@gmail.com>
2020-01-23 15:02:15 +0900

    impl test for gears_println et al

--- src/gitcgears.c
+++ src/gitcgears.c
@@ -2,7 +2,12 @@
 #include <curl/curl.h>
 #include <git2.h>
 
+#include "gears_util.h"
+
 int main(int argc, char** argv)
 {
-	printf("hello world\n");
+	gears_println("hello world", NULL);
+	gears_wrnln("hello world", NULL);
+	gears_errln("hello world", NULL);
+	return 0;
 }
