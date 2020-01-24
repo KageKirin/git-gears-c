3ff7e1018a77732ec8ba7cfc721ffc8d727334b6
KageKirin <kagekirin@gmail.com>
2020-01-24 20:23:01 +0900

    WIPTEST implement option parsing prototype

--- src/actions/foobar.c
+++ src/actions/foobar.c
@@ -6,6 +6,7 @@
 
 
 #include "gears_util.h"
+#include "gears_option.h"
 
 static char scrape[4096] = {0};
 
@@ -17,11 +18,48 @@ extern char git_buf__initbuf[];
 
 char git_buf__initbuf[1];
 
+static struct {
+	int explicit_value;
+	int implicit_value;
+	char* string_value;
+	char* argument1;
+	char* argument2;
+} OptionValues = {
+	.explicit_value = 0,
+	.implicit_value = 0,
+	.string_value = NULL,
+	.argument1 = "origin",
+	.argument2 = "master",
+};
+
+static Option options[] = {
+	{ 'e', "explicit", "explicitly set value", ^ int(int* argc, char*** argv){ OptionValues.explicit_value = atoi(*argv[1]); *argc -= 2; *argv+=2; return 2;}},
+	{ 'i', "implicit", "implicitly set value", ^ int(int* argc, char*** argv){ OptionValues.implicit_value = 1; *argc -= 1; *argv+=1; return 1;}},
+	{ 's', "string", "string value", ^ int(int* argc, char*** argv){ OptionValues.string_value = *argv[1]; *argc -= 2; *argv+=2; return 2;}},
+	{ .description = "argument value 1", ^ int(int* argc, char*** argv){ OptionValues.argument1 = *argv[0]; *argc-=1; *argv+=1; return 1;}},
+	{ .description = "argument value 2", ^ int(int* argc, char*** argv){ OptionValues.argument2 = *argv[0]; *argc-=1; *argv+=1; return 1;}},
+};
+
 
 int Foobar(int argc, char** argv)
 {
 	gears_println("foobar doing stuff", NULL);
-	// getopt or my own implementation to parse arguments
+
+	gears_println("explicit_value: %i", OptionValues.explicit_value);
+	gears_println("implicit_value: %i", OptionValues.implicit_value);
+	gears_println("string_value: %s", OptionValues.string_value);
+	gears_println("argument1: %s", OptionValues.argument1);
+	gears_println("argument2: %s", OptionValues.argument2);
+
+	// parse arguments
+	int newargc = parse_options(options, 5, argc-1, argv+1);
+
+	gears_println("explicit_value: %i", OptionValues.explicit_value);
+	gears_println("implicit_value: %i", OptionValues.implicit_value);
+	gears_println("string_value: %s", OptionValues.string_value);
+	gears_println("argument1: %s", OptionValues.argument1);
+	gears_println("argument2: %s", OptionValues.argument2);
+
 	// implementation of action
 	// result display
 
