4006889e8776b73b53a24c61fcf80fe346708288
Chris Helmich <kagekirin@gmail.com>
2020-02-20 10:47:55 +0900

    implement special case to handle -h/--help options

--- src/gears_option.c
+++ src/gears_option.c
@@ -3,18 +3,91 @@
 #include <stdlib.h>
 #include <string.h>
 
+extern char* program_name;
+char* action_name;
+
+static int showHelp(const Option* options, unsigned options_count)
+{
+	// header
+	gears_println("Help for %s %s", program_name, action_name);
+
+	// call help
+	gears_println("call:", NULL);
+	gears_printf("%s %s", program_name, action_name);
+	for (unsigned i = 0; i < options_count; ++i)
+	{
+		const char* val = options[i].consume == &gears_setOptionImplicit ? "" : " <value>";
+		if (options[i].shortname && options[i].longname)
+		{
+			gears_printf(" [-%c|--%s%s]", options[i].shortname, options[i].longname, val);
+		}
+		else if (options[i].shortname)
+		{
+			gears_printf(" [-%c%s]", options[i].shortname, val);
+		}
+		else if (options[i].longname)
+		{
+			gears_printf(" [--%s%s]", options[i].longname, val);
+		}
+		else
+		{
+			gears_printf(" <%s>", options[i].description);
+		}
+	}
+	gears_printf("\n\n", NULL);
+
+
+	// detail help
+	gears_println("details:", NULL);
+	gears_println("  -%c    --%-20s\t%s", 'h', "help", "show this message");
+	int positional_args = 0;
+	for (unsigned i = 0; i < options_count; ++i)
+	{
+		if (options[i].shortname && options[i].longname)
+		{
+			gears_println("  -%c    --%-20s\t%s", options[i].shortname, options[i].longname, options[i].description);
+		}
+		else if (options[i].shortname)
+		{
+			gears_println("  -%c      %-20s\t%s", options[i].shortname, "", options[i].description);
+		}
+		else if (options[i].longname)
+		{
+			gears_println("        --%-20s\t%s", options[i].longname, options[i].description);
+		}
+		else
+		{
+			gears_println("positional %i%-16s\t%s", ++positional_args, "", options[i].description);
+		}
+	}
+	return 0;
+}
 
 int parse_options(const Option* options, unsigned options_count, int argc, char** argv)
 {
+	// set action name
+	action_name = argv[0];
+	argc -= 1;
+	argv += 1;
+
 	// parse actual options (-, --) until '--' encountered or
 	while (argc > 0)
 	{
 		size_t len = strlen(argv[0]);
 
-		//!!! TODO: special case for -h/--help
+		// special case for -h/--help
+		if ((len == 2 && argv[0][0] == '-' && argv[0][1] == 'h') ||
+			(len == 6 && STRING_EQUALS("--help", &argv[0][0])))
+		{
+			gears_tag();
+			showHelp(options, options_count);
 
+			argc -= 1;
+			argv += 1;
+			break;
+		}
 		// single char arg
-		if (len == 2 && argv[0][0] == '-' && argv[0][1] != '-')
+		else if (len == 2 && argv[0][0] == '-' && argv[0][1] != '-')
 		{
 			char arg = argv[0][1];
 			for (unsigned i = 0; i < options_count; ++i)
