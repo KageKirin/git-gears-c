2d11babaa584d9beb12e06f2d0f798a8851e97eb
Chris Helmich <kagekirin@gmail.com>
2020-03-15 00:22:15 +0900

    add gears_giturl.h/c with gears_parseUrl()

---
+++ src/gears_giturl.h
@@ -0,0 +1,20 @@
+#ifndef GEARS_GITURL_H_INC
+#define GEARS_GITURL_H_INC
+
+// define a max length
+// as gitlab max to be on the safe side
+// github only allows up 100 characters on the webpage, and 64 in requests.
+// we use the same size to allow to long hostnames, as well as path names
+#define GEARS_GITURL_MAX_LENGTH 256
+
+typedef struct GitUrl
+{
+	char host[GEARS_GITURL_MAX_LENGTH];
+	char path[GEARS_GITURL_MAX_LENGTH];
+	char owner[GEARS_GITURL_MAX_LENGTH];
+	char reponame[GEARS_GITURL_MAX_LENGTH];
+} GitUrl;
+
+GitUrl gears_parseUrl(const char* url);
+
+#endif //GEARS_GITURL_H_INC
