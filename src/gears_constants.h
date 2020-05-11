5fb8c7ab3aeabe40abce1b954d28b2923bc9c52a
Chris Helmich <kagekirin@gmail.com>
2020-05-11 23:19:04 +0900

    add constants for config queries

---
+++ src/gears_constants.h
@@ -0,0 +1,9 @@
+#ifndef GEARS_CONSTANTS_H_INC
+#define GEARS_CONSTANTS_H_INC
+
+const char* kConfigAPIToken;
+const char* kConfigAPIType;
+const char* kConfigAPIEndpointGraphQL;
+const char* kConfigAPIEndpointRest;
+
+#endif // GEARS_CONSTANTS_H_INC
