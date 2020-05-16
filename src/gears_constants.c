5e7cd98ec7bf0b09482b3d8e23f265515ba59cd1
Chris Helmich <kagekirin@gmail.com>
2020-05-16 15:13:38 +0900

    add constants for API type

--- src/gears_constants.c
+++ src/gears_constants.c
@@ -4,3 +4,8 @@ const char* kConfigAPIToken = "token";
 const char* kConfigAPIType = "api";
 const char* kConfigAPIEndpointGraphQL = "url";
 const char* kConfigAPIEndpointRest = "rest";
+
+const char* kGearsAPIType[eGearsAPIType_max] = {
+	[eGearsAPIType_github] = "github",
+	[eGearsAPIType_gitlab] = "gitlab",
+};
