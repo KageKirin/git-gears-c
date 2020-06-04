73f33e8ed2412e9c19a77b7b6cfe4f96b5551ffd
Chris Helmich <kagekirin@gmail.com>
2020-06-04 23:08:58 +0900

    refactor http request handlers

--- src/gears_request.c
+++ src/gears_request.c
@@ -1,74 +1,58 @@
+#include "gears_request.h"
 #include "gears_util.h"
 
 #include <assert.h>
 #include <curl/curl.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 static size_t write_passthtrough(void* data, size_t size, size_t nmemb, void* userp)
 {
 	return size * nmemb;
 }
 
-typedef struct ReceiveBuffer
-{
-	char* buffer;
-	size_t maxSize;
-	size_t filledSize;
-} ReceiveBuffer;
-
-static size_t write_receive(void* data, size_t size, size_t nmemb, void* userp)
-{
-	ReceiveBuffer* recv = (ReceiveBuffer*)userp;
-	assert(recv);
-
-	if (recv->filledSize + size * nmemb < recv->maxSize)
-	{
-		memcpy(recv->buffer + recv->filledSize, data, size * nmemb);
-		recv->filledSize += size * nmemb;
-	}
-	return size * nmemb;
-}
-
-static size_t write_resize(void* data, size_t size, size_t nmemb, void* userp)
+static size_t write_response(void* data, size_t size, size_t nmemb, void* userp)
 {
 	gears_tag();
-	char** recv = (char**)userp;
-	assert(recv != NULL);
 
-	if (*recv)
+	GearsResponse* resp = (GearsResponse*)userp;
+	assert(resp);
+
+	const size_t dataSize = size * nmemb;
+
+	if (resp->buffer)
 	{
+		size_t growth = resp->size + dataSize;
+		char* new = resp->alloc(growth + 1);
+		assert(new);
 		gears_tag();
-
-		size_t len = strlen(*recv);
-		size_t growth = len + size * nmemb;
-		char* new = malloc(len + size * nmemb+1);
-		memset(new, 0, growth+1);
-		strncpy(new, *recv, len);
-		strncpy(new + len, data, size * nmemb);
-		free(*recv);
-		*recv = new;
+		memset(new, 0, growth + 1);
+		memcpy(new, resp->buffer, resp->size);
+		memcpy(new + resp->size, data, dataSize);
+		resp->free(resp->buffer);
+		resp->buffer = new;
+		resp->size = growth;
+		gears_tag();
 	}
 	else
 	{
+		char* new = resp->alloc(dataSize + 1);
+		assert(new);
 		gears_tag();
-
-		size_t growth = size * nmemb;
-		char* new = malloc(size * nmemb+1);
-		memset(new, 0, growth+1);
-		strncpy(new, data, size * nmemb);
-		*recv = new;
+		memset(new, 0, dataSize + 1);
+		memcpy(new, data, dataSize);
+		resp->buffer = new;
+		resp->size = dataSize;
+		gears_tag();
 	}
 
-	gears_tag();
-
-	return size * nmemb;
+	return dataSize;
 }
 
 
-int proto_getRequest(const char* url, const char* token)
+int gears_httpGet(const char* url, const char* token, GearsResponse* resp)
 {
 	assert(url);
 
@@ -76,8 +60,8 @@ int proto_getRequest(const char* url, const char* token)
 	if (curl)
 	{
 		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_passthtrough);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, resp ? write_response : write_passthtrough);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, resp);
 #ifdef SKIP_PEER_VERIFICATION
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 #endif
@@ -98,38 +82,7 @@ int proto_getRequest(const char* url, const char* token)
 }
 
 
-int proto_putRequest(const char* url, const char* token)
-{
-	assert(url);
-
-	CURL* curl = curl_easy_init();
-	if (curl)
-	{
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_PUT, 1L);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_passthtrough);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
-#ifdef SKIP_PEER_VERIFICATION
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-#endif
-#ifdef SKIP_HOSTNAME_VERIFICATION
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "git-gears-c/0.0.1");
-		if (token)
-		{
-			curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-			curl_easy_setopt(curl, CURLOPT_XOAUTH2_BEARER, token);
-		}
-		CURLcode res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-		return res;
-	}
-	return -1;
-}
-
-
-int proto_postRequest(const char* url, const char* token, const char* postData, char* recvData, int maxSize)
+int gears_httpPost(const char* url, const char* token, const char* postData, GearsResponse* resp)
 {
 	assert(url);
 
@@ -139,53 +92,9 @@ int proto_postRequest(const char* url, const char* token, const char* postData, 
 		curl_easy_setopt(curl, CURLOPT_URL, url);
 		curl_easy_setopt(curl, CURLOPT_POST, 1L);
 
-		ReceiveBuffer buffer = {
-			.buffer = recvData,
-			.maxSize = maxSize,
-			.filledSize = 0,
-		};
-
 		// on receive
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_receive);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-
-		// on send
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData); //<--- fill with post data
-#ifdef SKIP_PEER_VERIFICATION
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-#endif
-#ifdef SKIP_HOSTNAME_VERIFICATION
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "git-gears-c/0.0.1");
-		if (token)
-		{
-			curl_easy_setopt(curl, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-			curl_easy_setopt(curl, CURLOPT_XOAUTH2_BEARER, token);
-		}
-		CURLcode res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-		return res;
-	}
-	return -1;
-}
-
-
-int proto_postRequest2(const char* url, const char* token, const char* postData, char** recvData)
-{
-	assert(url);
-	gears_println("recvData: %p", recvData);
-	assert(recvData != NULL);
-
-	CURL* curl = curl_easy_init();
-	if (curl)
-	{
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_POST, 1L);
-
-		// on receive
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_resize);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, recvData);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, resp ? write_response : write_passthtrough);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, resp);
 
 		// on send
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData); //<--- fill with post data
--- src/gears_request.h
+++ src/gears_request.h
@@ -1,12 +1,25 @@
 #ifndef GEARS_REQUEST_H_INC
 #define GEARS_REQUEST_H_INC
 
-// prototypes
-// send HTTPs request via indicated method (get/put/post)
-// returns HTTP error code as number
-int proto_getRequest(const char* url, const char* token);
-int proto_putRequest(const char* url, const char* token);
-int proto_postRequest(const char* url, const char* token, const char* postData, char* recvData, int maxSize);
-int proto_postRequest2(const char* url, const char* token, const char* postData, char** recv);
+#include <stdlib.h>
+
+typedef struct GearsResponse
+{
+	char* buffer;
+	size_t size;
+
+	void* (*alloc)(size_t);
+	void (*free)(void*);
+} GearsResponse;
+
+//! send HTTP GET request
+//! returns curl error code
+//! if passed, GearsResponse used to be filled with the response data
+int gears_httpGet(const char* url, const char* token, GearsResponse* resp);
+
+//! send HTTP POST request
+//! returns curl error code
+//! if passed, GearsResponse used to be filled with the response data
+int gears_httpPost(const char* url, const char* token, const char* data, GearsResponse* resp);
 
 #endif // GEARS_REQUEST_H_INC
